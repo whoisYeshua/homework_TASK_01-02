0530e438194c296f83f8e388090bfeea548851a8
konstantinkuranov <alien1998@yandex.ru>
2020-02-18 23:06:19 +0300

    Complete TASK_01 and create package TASK_02 with TASK2_1

---
+++ src/TASK_02/TASK2_1.java
@@ -0,0 +1,11 @@
+package TASK_02;
+/* Stringbuilder с поддержкой операции undo
+Напишите свой класс StringBuilder с поддержкой операции undo. Для этого
+делегируйте все методы стандартному StringBuilder, а в собственном классе
+храните список всех операций для выполнения undo().*/
+
+public class TASK2_1 {
+    public static void main(String[] args) {
+
+    }
+}

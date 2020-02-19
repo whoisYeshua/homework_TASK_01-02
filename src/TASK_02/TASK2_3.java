17dbe771db82225043ec89584681b5d2201ca75f
konstantinkuranov <alien1998@yandex.ru>
2020-02-19 22:45:30 +0300

    Complete TASK2_3

--- src/TASK_02/TASK2_3.java
+++ src/TASK_02/TASK2_3.java
@@ -3,9 +3,61 @@ package TASK_02;
 Кельвины, Фаренгейты, и так далее. Все конвертации реализовать через
 имплементацию интерфейса и собственные классы.*/
 
+import java.util.Scanner;
+
 public class TASK2_3 {
     public static void main(String[] args) {
 
+        System.out.print("Введите температуру в °C, которую нужно перевести: ");
+        Scanner scan = new Scanner(System.in);
+        double celsius = scan.nextDouble();
+
+        double K = new Kelvin().getValue(celsius);
+        double F = new Fahrenheit().getValue(celsius);
+        double R = new Reaumur().getValue(celsius);
+
+
+        System.out.println("K = " + K);
+        System.out.println("°F = " + F);
+        System.out.println("°R = " + R);
+        System.out.println("°N = " + new Newton().getValue(celsius));
 
     }
 }
+
+interface Converter {
+    double getValue(double celsius);
+}
+
+class Kelvin implements Converter {
+
+    @Override
+    public double getValue(double celsius) {
+        return celsius + 273.15;
+    }
+}
+
+class Fahrenheit implements Converter {
+
+    @Override
+    public double getValue(double celsius) {
+        return 1.8 * celsius + 32;
+    }
+}
+
+class Reaumur implements Converter {
+
+    @Override
+    public double getValue(double celsius) {
+        return 0.8 * celsius;
+    }
+}
+
+class Newton implements Converter {
+
+    @Override
+    public double getValue(double celsius) {
+        return 0.33 * celsius;
+    }
+}
+
